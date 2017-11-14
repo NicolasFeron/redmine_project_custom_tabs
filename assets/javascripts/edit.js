43ffcf87fde404eac97d45de325df76354017e7c
Nicolas Feron <nfe@kantena.com>
2017-11-14 11:44:54 +0100

    Corrections des vues

--- assets/javascripts/edit.js
+++ assets/javascripts/edit.js
@@ -1,15 +1,10 @@
-var queries = document.getElementById("tab_setting_query_id");
-
-var url = document.getElementById("tab_setting_tab_url");
-
-queries.addEventListener("change", clear_url);
-
-url.addEventListener("change", clear_query);
-
-
-function clear_query() {
-	queries.selectedIndex = "0";
-}
-function clear_url() {
-	url.value = "";
-}
+$( document ).ready(function() {
+	var queries = $("#tab_setting_query_id");
+	var url = $("#tab_setting_tab_url");
+	queries.change(function() {
+		url.val("");
+	});
+	url.change(function() {
+		$('#tab_setting_query_id option:first').prop('selected',true);
+	});
+});	
