20bedec85e054a81bc1c50312f04b4cc7a1a97e3
Nicolas Feron <nfe@kantena.com>
2017-11-22 10:32:46 +0100

    Ajout d'exemples de modeles

--- app/helpers/tab_setting_helper.rb
+++ app/helpers/tab_setting_helper.rb
@@ -1,12 +1,8 @@
 module TabSettingHelper
-	def display_model_name tab_setting
-		tab_setting.tab_setting_model.nil?? (tab_setting.query.nil? ? "" : l(:default_tab_settings_model)) : tab_setting.tab_setting_model.name
-	end	
-
-	def display_query_name tab_setting
-		tab_setting.query.nil? ? "" : tab_setting.query.name
-	end
-
+	
+	################################
+	###          Data            ###
+	################################
 	def project_groups project
 		Group.joins(:members).where("members.project_id=#{project.id}")
 	end
@@ -19,14 +15,7 @@ module TabSettingHelper
 		projects
 	end
 
-	def time_entries_by_group(project,group,begin_date,end_date)
-		round(times_entries(project,begin_date,end_date).where("user_id in (#{group.users.map(&:id).join(',')})").sum(:hours)).to_s
-	end
-
-	def time_entries_by_project(project,begin_date,end_date)
-		round(times_entries(project,begin_date,end_date).sum(:hours)).to_s
-	end
-
+	
 	def times_entries(project,begin_date,end_date)
 		project.time_entries.where(spent_on: begin_date..end_date)
 	end 
@@ -40,17 +29,45 @@ module TabSettingHelper
 		end
 	end
 
+	################################
+	###          Display         ###
+	################################
+
+	def display_model_name tab_setting
+		tab_setting.tab_setting_model.nil?? (tab_setting.query.nil? ? "" : l(:default_tab_settings_model)) : tab_setting.tab_setting_model.name
+	end	
+
+	def display_query_name tab_setting
+		tab_setting.query.nil? ? "" : tab_setting.query.name
+	end
+
 	def display_times_entries_group_by_users_group(project,begin_date,end_date)
 		result = ""
 		project_groups(project).collect  do |group| 
 			time_entry = time_entries_by_group(project,group,begin_date, end_date)
 			if time_entry.to_i > 0
 				content_tag :tr do 
-					concat(content_tag :td, group.name, :class=> "one_tab left_align")
-					concat(content_tag :td, time_entry, :class=> "left_align")
+					concat(content_tag(:td, group.name, :class=> "one_tab left_align"))
+					concat(content_tag(:td, time_entry, :class=> "left_align"))
 				end
 			end
 		end.join.html_safe
+	end
+
+	def display_expand_button
+		content_tag(:span,"&nbsp;".html_safe, :class=> "expander", :onclick => "toggleRowGroup(this);")
+	end
+
+	################################
+	###       Calculation        ###
+	################################
+
+	def time_entries_by_group(project,group,begin_date,end_date)
+		round(times_entries(project,begin_date,end_date).where("user_id in (#{group.users.map(&:id).join(',')})").sum(:hours)).to_s
+	end
+
+	def time_entries_by_project(project,begin_date,end_date)
+		round(times_entries(project,begin_date,end_date).sum(:hours)).to_s
 	end
 
 	def round value, digit=2
